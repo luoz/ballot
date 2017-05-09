1d7d76c84a499b598dcbd0a54f416d7c1fbf3bb3
luoz <415589651@qq.com>
2017-05-09 16:06:28 +0800

    add def __unicode__ object explain

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,20 @@
 from __future__ import unicode_literals
 
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 class Question(models.Model):
-    questions_test = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __unicode__(self):
+        return self.question_text
 
 
 class Choice(models.Model):
@@ -14,4 +22,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def __unicode__(self):
+        return self.choice_text
 
