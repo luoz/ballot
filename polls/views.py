7b959118c2c26c60e2227b846bade695e1edd815
luoz <415589651@qq.com>
2017-05-11 18:17:57 +0800

    add detail results

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+#from django.http import Http404
 #from django.template import loader
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -29,10 +30,23 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    # response = "You're looking at the results of question %s."
+    # return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExit):
+        return render(request, 'polls/detail.html', {
+            'question': p, 'error_message': "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id, )))
+    #return HttpResponse("You're voting on question %s." % question_id)
 
