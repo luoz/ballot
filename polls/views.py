38ced1b68594078732c25a668dbd6d9366d25ac9
luoz <415589651@qq.com>
2017-05-12 09:44:11 +0800

    Edit vote except Choice.DoesNotExist

--- polls/views.py
+++ polls/views.py
@@ -40,9 +40,10 @@ def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExit):
+    except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
-            'question': p, 'error_message': "You didn't select a choice"
+            'question': p,
+            'error_message': "You didn't select a choice",
         })
     else:
         selected_choice.votes += 1
