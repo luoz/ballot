e73f6b67226ea14c28b82509c897c0d38dc7554a
luoz <415589651@qq.com>
2017-05-16 18:34:15 +0800

    add djaogo TestCase

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,120 @@
-from django.test import TestCase
+# polls/test.py
+# -*-coding: utf-8-*-
 
-# Create your tests here.
+import datetime
+
+from django.utils import timezone
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from .models import Question
+
+
+def create_question(question_text, days):
+    """
+    创建一个以question_text为标题，pub_date 为days天之后的问题
+    :param question_text:
+    :param days:
+    :return:
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_question(self):
+        """
+        如果数据库里没有投票， 应该显示一个合适的提示信息。
+        :return:
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_index_view_with_a_past_question(self):
+        """
+        pub_date 值是过去问题应该被显示在主页上。
+        :return:
+        """
+        create_question(question_text='Past question.', days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question.>'])
+
+    def test_index_view_with_a_future_question(self):
+        """
+        pub_date 值是将来的问题不应该被显示在主页上。
+        :return:
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.", status_code=200)
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_index_view_with_future_question_and_past_question(self):
+        """
+        如果数据库同时存在过去和将来的投票， 那么只应该显示过去那些。
+        :return:
+        """
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question.>'])
+
+    def test_index_view_with_two_past_questions(self):
+        """
+        目录页应该可以显示多个投票
+        :return:
+        """
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'], ['<Question: Past question 2.>','<Question: Past question 1.>']
+        )
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() 应该对pub_date字段值是将来那些问题返回 False
+        :return:
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        对于 pub_date 在一天以前的 Question， was_published_recently() 应该返回  False
+        :return:
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        对于 pub_date 在一天之内的 Question, was_published_recently() 应该返回 True
+        :return:
+        """
+        time = timezone.now() - datetime.timedelta(days=1)
+        recent_question = Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
+
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        访问将来发布的投票的详情页面应该会收到一个404 错误
+        :return:
+        """
+        future_question = create_question(question_text="Future question.", days=5)
+        response = self.client.get(reverse('polls:detail', args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        访问过去发布的投票详情页， 页面上应该显示投票标题
+        :return:
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        response = self.client.get(reverse('polls:detail', args=(past_question.id, )))
+        self.assertContains(response, past_question.question_text, status_code=200)
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-
+# -*- coding:utf-8 -*-
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
@@ -6,6 +6,7 @@ from django.core.urlresolvers import reverse
 #from django.template import loader
 from .models import Choice, Question
 from django.views import generic
+from django.utils import timezone
 
 #def index(request):
     #latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -23,7 +24,10 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+        #return Question.objects.order_by('pub_date')[:5]
 
 
 #def detail(request, question_id):
@@ -39,6 +43,13 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        过滤掉现在不应该被发布的投票
+        :return:
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 #def results(request, question_id):
     #question = get_object_or_404(Question, pk=question_id)
